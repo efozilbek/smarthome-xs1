c7e87d4086c38b8b770c68f36263eee87a6e39f3
ViktorMayer84@gmail.com <ViktorMayer84@gmail.com@43f048f2-a355-c5eb-eca0-2b2417a1b6c7>
2012-11-20 08:56:49 +0000

    Intents now can get sensor values

--- trunk/src/com/android/xs/controller/intent/XSIntent.java
+++ trunk/src/com/android/xs/controller/intent/XSIntent.java
@@ -1,9 +1,10 @@
 package com.android.xs.controller.intent;
 
 import java.util.ArrayList;
-
 import com.android.xs.controller.storage.PersistantStorage;
 import com.android.xs.controller.storage.RuntimeStorage;
+import com.android.xs.model.device.Actuator;
+import com.android.xs.model.device.Sensor;
 import com.android.xs.model.device.Xsone;
 import com.android.xs.model.device.components.AorS_Object;
 import com.android.xs.model.error.ConnectionException;
@@ -18,7 +19,7 @@ public class XSIntent extends Activity {
 	 * Private Variablen
 	 ***********************************************************************************************************************************************************/
 	Intent result_data = new Intent();
-	
+
 	/**
 	 * Konstruktoren
 	 ***********************************************************************************************************************************************************/
@@ -28,7 +29,7 @@ public class XSIntent extends Activity {
 	 ***********************************************************************************************************************************************************/
 
 	/**
-	 * wird beim Ausführen zuerst aufgerufen. 
+	 * wird beim Ausfï¿½hren zuerst aufgerufen.
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,14 +38,14 @@ public class XSIntent extends Activity {
 
 		ArrayList<String> names = getIntent().getStringArrayListExtra("names");
 		ArrayList<Integer> vals = getIntent().getIntegerArrayListExtra("vals");
-		
+
 		boolean invalid_data = false;
-		for (int val : vals){
+		for (int val : vals) {
 			if (val < 0 || val > 100)
 				invalid_data = true;
 		}
 		if (vals == null || names == null || (vals.size() != names.size()) || invalid_data) {
-			result_data.putExtra("Status", "Übergebene Daten Fehlerhaft!");
+			result_data.putExtra("Status", "ï¿½bergebene Daten Fehlerhaft!");
 			finish();
 			return;
 		}
@@ -53,14 +54,13 @@ public class XSIntent extends Activity {
 		// holen----------------------------------------------------
 		// Die RuntimeStorage mit Persistant Storage setzen
 		if (RuntimeStorage.getMyPers() == null)
-			RuntimeStorage.setMyPers(PersistantStorage
-					.getInstance(getBaseContext()));
+			RuntimeStorage.setMyPers(PersistantStorage.getInstance(getBaseContext()));
 
 		// Die Runtime Storage mit Http setzen
 		if (RuntimeStorage.getMyHttp() == null)
 			RuntimeStorage.setMyHttp();
 
-		// es wird versucht die gespeicherte IP anzulegen.. 
+		// es wird versucht die gespeicherte IP anzulegen..
 		String[] xsdata = RuntimeStorage.getXsdata();
 		if (xsdata == null) {
 			result_data.putExtra("Status", "XSone App noch nicht konfiguriert!");
@@ -69,8 +69,7 @@ public class XSIntent extends Activity {
 		}
 		try {
 			// beim anlegen entsteht netzlast, da alles ausgelesen wird
-			RuntimeStorage.setMyXsone(new Xsone(xsdata[0], xsdata[1],
-					xsdata[2]));
+			RuntimeStorage.setMyXsone(new Xsone(xsdata[0], xsdata[1], xsdata[2]));
 		} catch (ConnectionException e) {
 			result_data.putExtra("Status", "Verbindungsfehler!");
 			finish();
@@ -79,23 +78,28 @@ public class XSIntent extends Activity {
 
 		// Daten senden an das
 		// XS1-----------------------------------------------
+		ArrayList<String> Sens_vals = new ArrayList<String>();
 		for (int x = 0; x < names.size(); x++) {
-			AorS_Object obj = (AorS_Object) RuntimeStorage.getMyXsone()
-					.getObject(names.get(x));
-			if(obj != null && !obj.setValue(vals.get(x), true)){
-				getIntent().putExtra("Status", "Verbindungsfehler!");
-				finish();
-				return;
+			AorS_Object obj = (AorS_Object) RuntimeStorage.getMyXsone().getObject(names.get(x));
+			if (obj != null) {
+				if (obj.getClass().equals(Actuator.class)) {
+					if (!obj.setValue(vals.get(x), true)) {
+						getIntent().putExtra("Status", "Verbindungsfehler!");
+					}else
+						result_data.putExtra("Status", "Erfolgreich!");
+				} else if (obj.getClass().equals(Sensor.class)) {
+					Sens_vals.add(obj.getName()+ ";" + obj.getValue());
+					result_data.putExtra("Status", "Erfolgreich!");
+				}
 			}
-				
 		}
-		
+		result_data.putStringArrayListExtra("Values", Sens_vals);
 		result_data.putExtra("Status", "Erfolgreich!");
 		finish();
 		return;
 
 	}
-	
+
 	@Override
 	public void finish() {
 		setResult(RESULT_OK, result_data);
